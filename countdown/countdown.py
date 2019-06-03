04f5788ffc75c429d51cf00a22e43a5dc886ceb3
Austin Buscher <austin@Austins-Mac.frontierlocal.net>
2019-06-02 20:40:51 -0700

    pep 8

--- countdown/countdown.py
+++ countdown/countdown.py
@@ -6,11 +6,11 @@ from collections import defaultdict
 
 def good_order(input_list):
     """This function determines if all 2s and 3s come before all 0s and 1s in a list"""
-    hit23=False
+    hit23 = False
     for i in input_list:
         if i > 1:
             hit23 = True
-        if hit23 and i<2:
+        if hit23 and i < 2:
             return False
     return True
 
@@ -18,12 +18,13 @@ def good_order(input_list):
 # Global Constants, op and operator_list
 op = (operator.mul, operator.div, operator.add, operator.sub)
 
-operator_list = [] # Indices for operators, 0,1,2,3 is *,/,+,-
+operator_list = []  # Indices for operators, 0,1,2,3 is *,/,+,-
 for i4 in xrange(4 ** 5):
     cl = [i4 // 256 % 4, i4 // 64 % 4, i4 // 16 % 4, i4 // 4 % 4, i4 % 4]
     if good_order(cl):
         operator_list.append(cl)
 print len(operator_list)
+
 
 def gen_nums(loop_array):
     """Generate all possible numbers created from operations/rearrangements of a tuple"""
@@ -31,8 +32,7 @@ def gen_nums(loop_array):
 
     for line2 in loop_array:
         nums_index = sorted(line2)
-        nums_index = tuple([int(i) for i in nums_index]) #change to lambda
-
+        nums_index = tuple([int(i) for i in nums_index])
         for ops in operator_list:
             num = line2[0]
             for i in range(1, 6):
@@ -43,7 +43,7 @@ def gen_nums(loop_array):
 
 
 def write_file(nums, option_type):
-    with open('results/'+option_type+'.txt','w') as f:
+    with open('results/'+option_type+'.txt', 'w') as f:
         for num in nums:
             f.write(str(num)+';')
             for i in sorted(nums[num]):
@@ -71,12 +71,12 @@ def main():
         write_file(nums, option_type)
         print time.time() - start, " time elapsed\n"
 
-    #Laabcd
+    # Laabcd
     if option == 1 or all_options:
         option_type = "Laabcd"
         print option_type, option
         nums = {}
-        for s in itertools.combinations(s_array, 4): #does this need to be permutations?
+        for s in itertools.combinations(s_array, 4):
             for L in L_array:
                 for s0 in s:
                     loop_array = itertools.permutations([L] + list(s) + [s0])
@@ -84,19 +84,19 @@ def main():
         write_file(nums, option_type)
         print time.time() - start, " time elapsed\n"
 
-    #Laabbc
+    # Laabbc
     if option == 2 or all_options:
         option_type = "Laabbc"
         print option_type, option
         nums = {}
-        for s in itertools.permutations(s_array, 3): #does this need to be permutations?
+        for s in itertools.permutations(s_array, 3):
             for L in L_array:
-                loop_array = itertools.permutations([L] + list(s) + [s[0],s[1]])
+                loop_array = itertools.permutations([L] + list(s) + [s[0], s[1]])
                 nums.update(gen_nums(loop_array))
         write_file(nums, option_type)
         print time.time() - start, " time elapsed\n"
 
-    #LLabcd
+    # LLabcd
     if option == 3 or all_options:
         option_type = "LLabcd"
         print option_type, option
@@ -108,7 +108,7 @@ def main():
         write_file(nums, option_type)
         print time.time() - start, " time elapsed\n"
 
-    #LLaabc
+    # LLaabc
     if option == 4 or all_options:
         option_type = "LLaabc"
         print option_type, option
@@ -120,9 +120,8 @@ def main():
                     nums.update(gen_nums(loop_array))
         write_file(nums, option_type)
         print time.time() - start, " time elapsed\n"
-        #print count, ncr(4,2)*10*ncr(9,2)*720/2
 
-    #LLaabb
+    # LLaabb
     if option == 5 or all_options:
         option_type = "LLaabb"
         print option_type, option
@@ -134,7 +133,7 @@ def main():
         write_file(nums, option_type)
         print time.time() - start, " time elapsed\n"
 
-    #LLLabc
+    # LLLabc
     if option == 6 or all_options:
         option_type = "LLLabc"
         print option_type, option
@@ -146,7 +145,7 @@ def main():
         write_file(nums, option_type)
         print time.time() - start, " time elapsed\n"
 
-    #LLLaab
+    # LLLaab
     if option == 7 or all_options:
         option_type = "LLLaab"
         print option_type, option
@@ -159,7 +158,7 @@ def main():
         write_file(nums, option_type)
         print time.time() - start, " time elapsed\n"
 
-    #LLLLab
+    # LLLLab
     if option == 8 or all_options:
         option_type = "LLLLab"
         print option_type, option
@@ -170,12 +169,12 @@ def main():
         write_file(nums, option_type)
         print time.time()-start, " time elapsed\n"
 
-    #LLLLaa
+    # LLLLaa
     if option == 9 or all_options:
         option_type = "LLLLaa"
         nums = {}
         for a in s_array:
-            loop_array = itertools.permutations(L_array+[a,a])
+            loop_array = itertools.permutations(L_array+[a, a])
             nums.update(gen_nums(loop_array))
         write_file(nums, option_type)
         print time.time()-start, " time elapsed\n"
